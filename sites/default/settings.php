968d4ed301866ce995867b50838e10caa6899fe9
Riccardo Merenda <riccardomerenda@gmail.com>
2014-05-19 04:45:48 -0400

    installazione tema bootstrap3

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -210,7 +210,21 @@
  *   );
  * @endcode
  */
-$databases = array();
+$databases = array (
+  'default' => 
+  array (
+    'default' => 
+    array (
+      'database' => 'hci_test',
+      'username' => 'hci_test',
+      'password' => 'sistemicognitivi',
+      'host' => 'localhost',
+      'port' => '',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
 
 /**
  * Access control for update.php script.
@@ -242,7 +256,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = '';
+$drupal_hash_salt = '9o_BaluI2FxXl2oyekdDken9YmCUKK9gVhLWSjYzMDs';
 
 /**
  * Base URL (optional).
